1b8843f410f34baf3fb61ab76a792ac734a38a46
zoulvzhou <940933073@qq.com>
2018-03-04 12:04:16 +0800

    消息的keyvalue映射

---
+++ src/main/java/com/zou/keyvalue/MessageKeyAndValue.java
@@ -0,0 +1,110 @@
+package com.zou.keyvalue;
+
+import java.util.Map;
+
+/**
+ * @author zlz
+ * <p>
+ * 定义
+ */
+
+public class MessageKeyAndValue {
+
+    private Map<String, Object> messageKeyAndValue;
+
+    public Map<String, Object> getMessageKeyAndValue() {
+        return messageKeyAndValue;
+    }
+
+    public void setMessageKeyAndValue(Map<String, Object> messageKeyAndValue) {
+        this.messageKeyAndValue = messageKeyAndValue;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
