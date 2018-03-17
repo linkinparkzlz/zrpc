58cc0e5b87f23d20e36f6a0fde279e452630ee7c
zoulvzhou <940933073@qq.com>
2018-03-17 16:50:49 +0800

    使用Protobuf的编码器类

---
+++ src/main/java/com/zou/serializable/protostuff/ProtostuffEncoder.java
@@ -0,0 +1,11 @@
+package com.zou.serializable.protostuff;
+
+import com.zou.serializable.message.MessageCodeHelper;
+import com.zou.serializable.message.MessageEncoder;
+
+public class ProtostuffEncoder extends MessageEncoder {
+
+    public ProtostuffEncoder(MessageCodeHelper helper) {
+        super(helper);
+    }
+}
