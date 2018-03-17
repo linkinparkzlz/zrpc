92cb7efec7285d5032639400786cf1a9ad5c6c11
zoulvzhou <940933073@qq.com>
2018-03-17 17:26:46 +0800

    接收消息的处理器

---
+++ src/main/java/com/zou/netty/messageHandler/MessageReceiveHandler.java
@@ -0,0 +1,43 @@
+package com.zou.netty.messageHandler;
+
+import com.zou.bean.MessageRequest;
+import com.zou.bean.MessageResponse;
+import com.zou.netty.executor.MessageReceiveExecutor;
+import com.zou.netty.initializer.ReceiveInitializeFacade;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class MessageReceiveHandler extends ChannelInboundHandlerAdapter {
+    private final Map<String, Object> map;
+
+    public MessageReceiveHandler(Map<String, Object> map) {
+        this.map = map;
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        MessageRequest request = (MessageRequest) msg;
+        MessageResponse response = new MessageResponse();
+
+        ReceiveInitializeFacade facade = new ReceiveInitializeFacade(request, response, map);
+
+        Callable<Boolean> receveiveTask = facade.getTask();
+
+        MessageReceiveExecutor.submit(receveiveTask, ctx, request, response);
+
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+        cause.printStackTrace();
+        ctx.close();
+
+    }
+}
