947fff5c184d2b3f4dd4f6065df5889c25850ddc
zoulvzhou <940933073@qq.com>
2018-03-17 16:25:40 +0800

    Hessian编码类

---
+++ src/main/java/com/zou/serializable/hessian/HessianEncoder.java
@@ -0,0 +1,11 @@
+package com.zou.serializable.hessian;
+
+import com.zou.serializable.message.MessageCodeHelper;
+import com.zou.serializable.message.MessageEncoder;
+
+public class HessianEncoder extends MessageEncoder {
+
+    public HessianEncoder(MessageCodeHelper helper) {
+        super(helper);
+    }
+}
