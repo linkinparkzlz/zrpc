390599290cc4be5226a7f45b70aea0199d84c3f4
zoulvzhou <940933073@qq.com>
2018-03-17 16:26:21 +0800

    Hessian解码类

---
+++ src/main/java/com/zou/serializable/hessian/HessianDecoder.java
@@ -0,0 +1,15 @@
+package com.zou.serializable.hessian;
+
+import com.zou.serializable.message.MessageCodeHelper;
+import com.zou.serializable.message.MessageDecoder;
+
+public class HessianDecoder extends MessageDecoder {
+
+
+    public HessianDecoder(MessageCodeHelper helper) {
+
+        super(helper);
+    }
+
+
+}
